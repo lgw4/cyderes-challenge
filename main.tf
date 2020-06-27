89f52823ad567dc13cd44a500607868506a1dffb
Chip Warden <lgw4@icloud.com>
2020-06-27 18:08:26 -0500

    Minor changes.

--- main.tf
+++ main.tf
@@ -6,10 +6,8 @@ module "networking" {
 }
 
 module "elasticsearch" {
-  source         = "./modules/elasticsearch"
-  namespace      = var.namespace
-  private_subnet = var.private_subnet
-  public_subnet  = var.public_subnet
-  vpc            = module.networking.vpc
-  sg             = module.networking.sg
+  source    = "./modules/elasticsearch"
+  namespace = var.namespace
+  vpc       = module.networking.vpc
+  sg        = module.networking.sg
 }
--- modules/elasticsearch/variables.tf
+++ modules/elasticsearch/variables.tf
@@ -2,24 +2,14 @@ variable "namespace" {
   type = string
 }
 
+variable "region" {
+  type = string
+}
+
 variable "sg" {
   type = any
 }
 
 variable "vpc" {
   type = any
-}
-
-variable "region" {
-  description = "AWS region"
-  default     = "us-east-1"
-  type        = string
-}
-
-variable "private_subnet" {
-  type = string
-}
-
-variable "public_subnet" {
-  type = string
 }
